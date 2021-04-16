7ebe69185154f17e3e9f966e4db3560e3dc7a2f6
George <grob6000@users.noreply.github.com>
2021-04-16 22:00:51 +1000

    Install script

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
+#!/bin/sh
 sudo apt-get install git
+cd ~
+git clone https://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@github.com/grob6000/JJClock
+cd ~/JJClock
+cp ./jjclock.service /etc/systemd/system/
+sudo chmod 644 /etc/systemd/system/jjclock.service
+sudo systemctl daemon-reload
+sudo systemctl enable jjclock.service
+sudo systemctl start jjclock.service
