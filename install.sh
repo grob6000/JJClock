33ac50139a49a30bca331fad34bda3b7ca8960f8
George <grob6000@users.noreply.github.com>
2021-04-21 09:57:10 +1000

    Update install script

--- install.sh
+++ install.sh
@@ -3,16 +3,14 @@
 # install required programs
 sudo apt-get -y install git
 sudo apt-get -y install python3
-sudo apt-get -y install pip3
-sudo apt-get -y install isc-dhcp-server
+sudo apt-get -y install hostapd
+sudo apt-get -y install dnsmasq
+sudo apt-get -y install netfilter-persistent iptables-persistent
 
 # python packages
 pip3 install pygame
 pip3 install gpiozero
 pip3 install pillow
-
-# set up dhcp
-# set up ap
 
 # download/update
 cd ~
@@ -20,6 +18,21 @@ git clone https://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@github.com/grob6000/J
 cd ~/JJClock
 git checkout main
 git pull --force
+
+mkdir ./oldconfig
+
+# set up hostapd
+cp /etc/hostapd/hostapd.conf ./oldconfig/hostapd.conf
+sudo rm /etc/hostapd/hostapd.conf
+sudo cp ./hostapd.conf /etc/hostapd/hostapd.conf
+sudo sed -i '/DAEMON_CONF=/c\DAEMON_CONF=/etc/hostapd/hostapd.conf' /etc/init.d/hostapd
+sudo systemctl disable hostapd.service # disabled by default
+
+# set up dnsmasq
+cp /etc/dnsmasq.conf ./oldconfig/dnsmasq.conf
+sudo rm /etc/dnsmasq.conf
+sudo cp ./dnsmasq.conf /etc/dnsmasq.conf
+sudo systemctl disable dnsmasq.service # disabled by default
 
 # set up script as service, run on boot
 sudo cp ./jjclock.service /etc/systemd/system/
