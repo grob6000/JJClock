d9a0d2723c6602ce9b96b692ba7040dddd4ed5ab
George <grob6000@users.noreply.github.com>
2021-04-21 11:53:05 +1000

    debug in clientmode.sh

--- clientmode.sh
+++ clientmode.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-sudo systemctl stop hostapd.service
-sudo systemctl stop dnsmasq.service
+sudo systemctl stop hostapd.service 2<&1 | tee -a debug.log
+sudo systemctl stop dnsmasq.service 2<&1 | tee -a debug.log
 #sudo ip link set dev wlan0 down
-sudo ip addr flush dev wlan0
-wpa_cli enable network 0
-sudo dhclient
+sudo ip addr flush dev wlan0 2<&1 | tee -a debug.log
+wpa_cli enable network 0 2<&1 | tee -a debug.log
+sudo dhclient -v 2<&1 | tee -a debug.log
