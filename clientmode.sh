00a1860e0636efb699d87050a1fd2d00b261375b
George <grob6000@users.noreply.github.com>
2021-04-21 11:58:35 +1000

    clientmode define iface

--- clientmode.sh
+++ clientmode.sh
@@ -4,4 +4,4 @@ sudo systemctl stop dnsmasq.service 2<&1 | tee -a debug.log
 #sudo ip link set dev wlan0 down
 sudo ip addr flush dev wlan0 2<&1 | tee -a debug.log
 wpa_cli enable network 0 2<&1 | tee -a debug.log
-sudo dhclient -v 2<&1 | tee -a debug.log
+sudo dhclient wlan0 -v 2<&1 | tee -a debug.log
