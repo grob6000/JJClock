9746c76c4929b31a362a55faf257a5c0cad046f9
George <grob6000@users.noreply.github.com>
2021-04-21 12:03:00 +1000

    release and renew

--- clientmode.sh
+++ clientmode.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-sudo systemctl stop hostapd.service 2<&1 | tee -a debug.log
-sudo systemctl stop dnsmasq.service 2<&1 | tee -a debug.log
+sudo systemctl stop hostapd.service 2<&1 | tee clientmodedebug.log
+sudo systemctl stop dnsmasq.service 2<&1 | tee -a clientmodedebug.log
 #sudo ip link set dev wlan0 down
-sudo ip addr flush dev wlan0 2<&1 | tee -a debug.log
-wpa_cli enable network 0 2<&1 | tee -a debug.log
-sudo dhclient wlan0 -v 2<&1 | tee -a debug.log
+sudo ip addr flush dev wlan0 2<&1 | tee -a clientmodedebug.log
+wpa_cli enable network 0 2<&1 | tee -a clientmodedebug.log
+sudo dhclient -r wlan0 -v 2<$1 | tee -a clientmodedebug.log
+sudo dhclient wlan0 -v 2<&1 | tee -a clientmodedebug.log
