fff22259a5fa94b808e61da7c434816954f6d23f
George <grob6000@users.noreply.github.com>
2021-04-21 16:03:37 +1000

    Update jjclock.py

--- jjclock.py
+++ jjclock.py
@@ -79,8 +79,8 @@ def testDisplay(gridsize=100):
   global epddisplay
   print("test image for display")
   epddisplay.frame_buf.paste(0xFF, box=(0,0,epddisplay.width,epddisplay.height)) # fill white
-  for y in range(0,int(epddisplay.height/gridsize)):
-    for x in range(0,int(epddisplay.width/gridsize)):
+  for y in range(0,int(epddisplay.height/gridsize)+1):
+    for x in range(0,int(epddisplay.width/gridsize)+1):
       if (x+y)%2==0: # make an alternating pattern
         epddisplay.frame_buf.paste(0x00, box=(x*gridsize,y*gridsize,min((x+1)*gridsize-1,epddisplay.width),min((y+1)*gridsize-1,epddisplay.height))) # draw black squares
   epddisplay.draw_full(constants.DisplayModes.GC16)
