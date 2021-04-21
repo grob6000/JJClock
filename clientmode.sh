c016bcb5f8fbdfde273eabd6a7bdcf779410abb2
George <grob6000@users.noreply.github.com>
2021-04-21 12:07:20 +1000

    remove clientmode debug

--- clientmode.sh
+++ clientmode.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-sudo systemctl stop hostapd.service 2<&1 | tee clientmodedebug.log
-sudo systemctl stop dnsmasq.service 2<&1 | tee -a clientmodedebug.log
+sudo systemctl stop hostapd.service
+sudo systemctl stop dnsmasq.service
 #sudo ip link set dev wlan0 down
-sudo ip addr flush dev wlan0 2<&1 | tee -a clientmodedebug.log
-wpa_cli enable network 0 2<&1 | tee -a clientmodedebug.log
-sudo dhclient -r wlan0 -v 2<$1 | tee -a clientmodedebug.log
-sudo dhclient wlan0 -v 2<&1 | tee -a clientmodedebug.log
+sudo ip addr flush dev wlan0
+wpa_cli enable network 0
+sudo dhclient -r wlan0
+sudo dhclient wlan0
