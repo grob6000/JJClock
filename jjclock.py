1103d0b77fd44a91af6fa50a7d1bfa91c0015bcd
George <grob6000@users.noreply.github.com>
2021-04-22 14:10:52 +1000

    Update jjclock.py

--- jjclock.py
+++ jjclock.py
@@ -213,12 +213,12 @@ userbutton = Button(buttongpio, bounce_time=debounce/1000.0)
 userbutton.when_pressed = onButton
 
 # init pygame
-print("init pygame engine")
-os.environ["SDL_VIDEODRIVER"] = "dummy"
-pygame.init()
+#print("init pygame engine")
+#os.environ["SDL_VIDEODRIVER"] = "dummy"
+#pygame.init()
 #screen = pygame.display.set_mode(screensize)
 #pygame.display.set_caption("My Game")
-clock = pygame.time.Clock() # Used to manage how fast the screen updates
+#clock = pygame.time.Clock() # Used to manage how fast the screen updates
 
 # init epd display
 print("init display")
