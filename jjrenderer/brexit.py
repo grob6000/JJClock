ff3c50cab632ae06036e45964bb0bccab5f73476
George <grob6000@users.noreply.github.com>
2021-04-27 22:04:49 +1000

    Update brexit.py

--- jjrenderer/brexit.py
+++ jjrenderer/brexit.py
@@ -6,7 +6,7 @@ class RendererBrexitClock(Renderer):
   def getMenuItem(self):
     return {"icon":"brexit.png","text":"Brexit"}
   def doRender(self, screen, **kwargs):
-    return super().doRender(self,screen,**kwargs)
+    return super().doRender(screen,**kwargs)
 
 if __name__ == "__main__":
   testRenderer(RendererBrexitClock)
