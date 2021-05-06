e79768a78aa2b01f1a162fb1f67660953975882a
George <grob6000@users.noreply.github.com>
2021-05-06 21:00:37 +1000

    don't need to disable

--- apmode.sh
+++ apmode.sh
@@ -2,7 +2,7 @@
 wpa_cli disable_network 0
 sudo ip link set dev wlan0 down
 sudo ip addr add 192.168.99.1/25 dev wlan0
-sudo systemctl enable dnsmasq.service
+#sudo systemctl enable dnsmasq.service
 sudo systemctl restart dnsmasq.service
-sudo systemctl enable hostapd.service
+#sudo systemctl enable hostapd.service
 sudo systemctl restart hostapd.service
