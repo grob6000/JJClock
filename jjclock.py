69dc223dcaaf07d8bdc30d6c8c77abfdfc96c90e
George <grob6000@users.noreply.github.com>
2021-05-06 16:28:42 +1000

    Update jjclock.py

--- jjclock.py
+++ jjclock.py
@@ -213,7 +213,7 @@ def checkForUpdate():
   myname = ""
   if "linux" in sys.platform:
     try:
-      myname = subprocess.check_output(["git", "describe", "--tags", "--abbrev=0"])
+      myname = subprocess.run(["git", "describe", "--tags", "--abbrev=0"], text=True, capture_output=True).stdout
     except subprocess.CalledProcessError:
       logging.warning("unknown version. will update.")
     
