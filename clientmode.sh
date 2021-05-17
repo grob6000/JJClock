705a86be315bf5d517131b6f51fdb71393a50f13
George <grob6000@users.noreply.github.com>
2021-05-18 09:16:27 +1000

    Update clientmode.sh

--- clientmode.sh
+++ clientmode.sh
@@ -3,9 +3,11 @@ sudo systemctl stop hostapd.service
 #sudo systemctl disable hostapd.service
 sudo systemctl stop dnsmasq.service
 #sudo systemctl disable dnsmasq.service
-#sudo ip link set dev wlan0 down
+sudo ip link set dev wlan0 down
 sudo ip addr flush dev wlan0
 #wpa_cli enable -i wlan0 network 0
 wpa_cli -i wlan0 reconnect
+sudo systemctl daemon-reload
+sudo systemctl restart dhcpd.service
 #sudo dhclient -r wlan0
 sudo dhclient wlan0
