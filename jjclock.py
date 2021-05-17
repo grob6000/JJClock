4f7ae093fe95f2e86d6679aca636d9b8f3322401
George <grob6000@users.noreply.github.com>
2021-05-17 20:08:58 +1000

    put wifi stuff into module

--- jjclock.py
+++ jjclock.py
@@ -14,7 +14,6 @@ import timezonefinder
 from PIL import Image, ImageDraw, ImageFont
 import sys
 from github import Github
-import re
 
 if "linux" in sys.platform:
   from gpiozero import Device, Button
@@ -32,6 +31,7 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 import jjrenderer
 import gpshandler
+import wifimanager
 
 ## CONSTANTS ##
 
@@ -112,27 +112,6 @@ def onMenuTimeout():
   logging.info("menu timeout")
   changeMode(menu[menuitemselected].getName())
 
-def setWifiMode(newwifimode):
-  global currentwifimode
-  if (newwifimode == currentwifimode):
-    logging.info("wifi mode unchanged")
-  elif newwifimode == "ap":
-    logging.info("wifi mode AP")
-    try:
-      subprocess.run(["bash", os.path.join(scriptpath, "apmode.sh")], check=True)
-    except subprocess.CalledProcessError:
-      logging.error("unsuccessful running apmode.sh")
-    currentwifimode = newwifimode
-  elif newwifimode == "client":
-    logging.info("wifi mode Client")
-    try:
-      subprocess.run(["bash", os.path.join(scriptpath, "clientmode.sh")], check=True)
-    except subprocess.CalledProcessError:
-      logging.error("unsuccessful running clientmode.sh")
-    currentwifimode = newwifimode
-  else:
-    logging.info("invalid wifi mode, no change")
-
 def savePersistentMode(mode):
   logging.info("NOT IMPLEMENTED - persist mode as file")
 
@@ -156,14 +135,14 @@ def changeMode(mode):
     currentmode = mode
     if mode == "config":
       # set wifi to AP mode
-      setWifiMode("ap")
+      wifimanager.setWifiMode("ap")
       gpsstat = gpshandler.getStatus()
       kwargs["ssid"] = ap_ssid
       kwargs["password"] = ap_pass
       kwargs["ip"] = formatIP(ip_addr)
       kwargs["gpsstat"] = gpsstat
     else:
-      setWifiMode("client") # all other modes should be in client state (if no wifi configured, will be disconnected...)
+      wifimanager.setWifiMode("client") # all other modes should be in client state (if no wifi configured, will be disconnected...)
     if mode == "menu":
         kwargs["selecteditem"] = menuitemselected
         kwargs["menu"] = menu
@@ -268,87 +247,6 @@ def doUpdate(wgeturl, tag):
   if updateok:
     quit()
 
-examplenetwork = {"index":0,"ssid":"examplessid","psk":"password123","bssid":"00:11:22:33:44:55", "frequency":"2462", "rssi":-71, "flags":["WPA2-PSK-CCMP","ESS"]}
-
-def getNetworks():
-  networks = []
-  if "linux" in sys.platform:
-    for i in range(0,10): # why would anyone configure more than 10???? yes magic numbers well whoopdeedoo
-      cp = subprocess.run(["wpa_cli", "-i", iface, "get_network", str(i), "ssid"], capture_output=True, text=True)
-      if "FAIL" in cp.stdout:
-        break
-      else:
-        network = {"id":i, "ssid":cp.stdout.strip('" \n')}
-        # for now, don't need anything else
-        networks.append(network)
-  else:
-    logging.error("cannot access wifi config")
-  return networks
-    
-def scanNetworks():
-  scannetworks = []
-  if "linux" in sys.platform:
-    cp = subprocess.run(["wpa_cli", "-i", iface, "scan"], capture_output=True, text=True)
-    if "OK" in cp.stdout:
-      cp2 = subprocess.run(["wpa_cli", "-i", iface, "scan_results"], capture_output=True, text=True)
-      if cp2.returncode == 0:
-        lines = cp2.stdout.strip().split("\n")
-        for l in lines:
-          if len(l) > 0 and not l.startswith("bssid"):
-            parts = l.split(None,4)
-            flags = parts[3].strip("[]").split("][")
-            if len(parts)==5: # has SSID (4-part entries have blank SSID)
-              network = {"bssid":parts[0],"freq":int(parts[1]),"rssi":int(parts[2]),"flags":flags,"ssid":parts[4]}
-              scannetworks.append(network)
-      else:
-        logging.error("could not retrieve scanned networks")
-    else:
-      logging.error("could not scan wifi networks")
-  else:
-    logging.error("cannot access wifi config")
-  return scannetworks
-
-def removeNetwork(netindex):
-  # remove network and save config
-  if "linux" in sys.platform:
-    cp = subprocess.run(["wpa_cli", "-i", iface, "remove_network", str(netindex)], capture_output=True, text=True)
-    if not "OK" in cp.stdout:
-      logging.error("could not delete network " + str(netindex))
-    cp = subprocess.run(["wpa_cli", "-i", iface, "save_config"], capture_output=True, text=True)
-    if not "OK" in cp.stdout:
-      logging.error("error saving wifi config")
-  else:
-    logging.error("cannot access wifi config")
-    
-def addNetwork(ssid, psk):
-  netindex = -1
-  # add network
-  if "linux" in sys.platform:
-    cp = subprocess.run(["wpa_cli", "-i", iface, "add_network"], capture_output=True, text=True)
-    if cp.returncode == 0:
-      netindex = int(cp.stdout.strip())
-      logging.debug("netindex={0}".format(netindex))
-      allok = True
-      cp2 = subprocess.run(["wpa_cli", "-i", iface, "set_network", str(netindex), "ssid", "\""+str(ssid)+"\""], capture_output=True, text=True)
-      if "FAIL" in cp2.stdout:
-        allok = False
-        logging.debug("set ssid fail: " + cp2.stdout)
-      cp2 = subprocess.run(["wpa_cli", "-i", iface, "set_network", str(netindex), "psk", "\""+str(psk)+"\""], capture_output=True, text=True)
-      if "FAIL" in cp2.stdout:
-        allok = False
-        logging.debug("set psk fail: " + cp2.stdout)
-      if allok:
-        cp = subprocess.run(["wpa_cli", "-i", iface, "save_config"], capture_output=True, text=True)
-        if not "OK" in cp.stdout:
-          logging.error("error saving wifi config")
-      else:
-        logging.error("error setting ssid/psk for wifi")
-    else:
-      logging.error("error adding wifi network")
-  else:
-    logging.error("cannot access wifi config")
-  return netindex
-    
 ## SCRIPT ##
 if __name__ == "__main__":
 
@@ -368,28 +266,24 @@ if __name__ == "__main__":
   else:
     logging.warning("no display on this platform.")
     epddisplay = None
-  
-  
-  # wifi setup
-  #logging.debug(getNetworks())
-
-  # now screen is running, check for update
-  #checkForUpdate()
 
   # wifi testing
-  n = getNetworks()
+  n = wifimanager.getNetworks()
   print(n)
   
-  sn = scanNetworks()
+  sn = wifimanager.scanNetworks()
   print(sn)
   
-  i = addNetwork("test", "nobigdeal")
-  n = getNetworks()
+  i = wifimanager.addNetwork("test", "nobigdeal")
+  n = wifimanager.getNetworks()
   print(n)
   
-  removeNetwork(i)
-  n = getNetworks()
+  wifimanager.removeNetwork(i)
+  n = wifimanager.getNetworks()
   print(n)
+  
+  # now screen is running, check for update
+  #checkForUpdate()
   
   quit()
   
---
+++ wifimanager.py
@@ -0,0 +1,104 @@
+import sys
+import subprocess
+import logging
+from jjcommon import *
+
+def getNetworks():
+  networks = []
+  if "linux" in sys.platform:
+    for i in range(0,10): # why would anyone configure more than 10???? yes magic numbers well whoopdeedoo
+      cp = subprocess.run(["wpa_cli", "-i", iface, "get_network", str(i), "ssid"], capture_output=True, text=True)
+      if "FAIL" in cp.stdout:
+        break
+      else:
+        network = {"id":i, "ssid":cp.stdout.strip('" \n')}
+        # for now, don't need anything else
+        networks.append(network)
+  else:
+    logging.error("cannot access wifi config")
+  return networks
+  
+def scanNetworks():
+  scannetworks = []
+  if "linux" in sys.platform:
+    cp = subprocess.run(["wpa_cli", "-i", iface, "scan"], capture_output=True, text=True)
+    if "OK" in cp.stdout:
+      cp2 = subprocess.run(["wpa_cli", "-i", iface, "scan_results"], capture_output=True, text=True)
+      if cp2.returncode == 0:
+        lines = cp2.stdout.strip().split("\n")
+        for l in lines:
+          if len(l) > 0 and not l.startswith("bssid"):
+            parts = l.split(None,4)
+            flags = parts[3].strip("[]").split("][")
+            if len(parts)==5: # has SSID (4-part entries have blank SSID)
+              network = {"bssid":parts[0],"freq":int(parts[1]),"rssi":int(parts[2]),"flags":flags,"ssid":parts[4]}
+              scannetworks.append(network)
+      else:
+        logging.error("could not retrieve scanned networks")
+    else:
+      logging.error("could not scan wifi networks")
+  else:
+    logging.error("cannot access wifi config")
+  return scannetworks
+
+def removeNetwork(netindex):
+  # remove network and save config
+  if "linux" in sys.platform:
+    cp = subprocess.run(["wpa_cli", "-i", iface, "remove_network", str(netindex)], capture_output=True, text=True)
+    if not "OK" in cp.stdout:
+      logging.error("could not delete network " + str(netindex))
+    cp = subprocess.run(["wpa_cli", "-i", iface, "save_config"], capture_output=True, text=True)
+    if not "OK" in cp.stdout:
+      logging.error("error saving wifi config")
+  else:
+    logging.error("cannot access wifi config")
+  
+def addNetwork(ssid, psk):
+  netindex = -1
+  # add network
+  if "linux" in sys.platform:
+    cp = subprocess.run(["wpa_cli", "-i", iface, "add_network"], capture_output=True, text=True)
+    if cp.returncode == 0:
+      netindex = int(cp.stdout.strip())
+      logging.debug("netindex={0}".format(netindex))
+      allok = True
+      cp2 = subprocess.run(["wpa_cli", "-i", iface, "set_network", str(netindex), "ssid", "\""+str(ssid)+"\""], capture_output=True, text=True)
+      if "FAIL" in cp2.stdout:
+        allok = False
+        logging.debug("set ssid fail: " + cp2.stdout)
+      cp2 = subprocess.run(["wpa_cli", "-i", iface, "set_network", str(netindex), "psk", "\""+str(psk)+"\""], capture_output=True, text=True)
+      if "FAIL" in cp2.stdout:
+        allok = False
+        logging.debug("set psk fail: " + cp2.stdout)
+      if allok:
+        cp = subprocess.run(["wpa_cli", "-i", iface, "save_config"], capture_output=True, text=True)
+        if not "OK" in cp.stdout:
+          logging.error("error saving wifi config")
+      else:
+        logging.error("error setting ssid/psk for wifi")
+    else:
+      logging.error("error adding wifi network")
+  else:
+    logging.error("cannot access wifi config")
+  return netindex
+  
+def setWifiMode(newwifimode):
+  global currentwifimode
+  if (newwifimode == currentwifimode):
+    logging.info("wifi mode unchanged")
+  elif newwifimode == "ap":
+    logging.info("wifi mode AP")
+    try:
+      subprocess.run(["bash", os.path.join(scriptpath, "apmode.sh")], check=True)
+    except subprocess.CalledProcessError:
+      logging.error("unsuccessful running apmode.sh")
+    currentwifimode = newwifimode
+  elif newwifimode == "client":
+    logging.info("wifi mode Client")
+    try:
+      subprocess.run(["bash", os.path.join(scriptpath, "clientmode.sh")], check=True)
+    except subprocess.CalledProcessError:
+      logging.error("unsuccessful running clientmode.sh")
+    currentwifimode = newwifimode
+  else:
+    logging.info("invalid wifi mode, no change")
