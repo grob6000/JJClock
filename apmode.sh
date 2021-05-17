9251f1feab5e59be3759bdede1e13c60e35ab5bf
George <grob6000@users.noreply.github.com>
2021-05-17 21:41:07 +1000

    wpa_cli commands fix in modechange scripts

--- apmode.sh
+++ apmode.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-wpa_cli disable_network 0
+#wpa_cli -i wlan0 disable_network 0
+wpa_cli -i wlan0 disconnect
 sudo ip link set dev wlan0 down
 sudo ip addr add 192.168.99.1/25 dev wlan0
 #sudo systemctl enable dnsmasq.service
--- clientmode.sh
+++ clientmode.sh
@@ -5,6 +5,7 @@ sudo systemctl stop dnsmasq.service
 #sudo systemctl disable dnsmasq.service
 #sudo ip link set dev wlan0 down
 sudo ip addr flush dev wlan0
-wpa_cli enable network 0
+#wpa_cli enable -i wlan0 network 0
+wpa_cli -i wlan0 reconnect
 sudo dhclient -r wlan0
 sudo dhclient wlan0
