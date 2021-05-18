b3946e2146dea7920b747ec8be1d6143b91e84c6
George <grob6000@users.noreply.github.com>
2021-05-19 09:40:57 +1000

    Update config.py

--- jjrenderer/config.py
+++ jjrenderer/config.py
@@ -17,19 +17,25 @@ class RendererConfig(Renderer):
     t = "Configuration"
     tsz = RendererConfig.bigfont.getsize(t)
     configicon = getImage("config")
-    x = int((screen.size[0] - configicon.size[0] - tsz[0] - 50) / 2)
-    screen.paste(configicon, (x, 100))
-    x = x + configicon.size[0] + 50
+    x0 = int((screen.size[0] - configicon.size[0] - tsz[0] - 50) / 2)
+    y0 = 200
+    screen.paste(configicon, (x0, y0))
+    x = x0 + configicon.size[0] + 50
     draw = ImageDraw.Draw(screen)
-    draw.text((x, int(100 + configicon.size[1]/2 - tsz[1]/2)), t, font=RendererConfig.bigfont, fill=0x00)
+    draw.text((x, int(y0 + configicon.size[1]/2 - tsz[1]/2)), t, font=RendererConfig.bigfont, fill=0x00)
     
     # data
     if kwargs["port"] == 80:
-      addr = kwargs["ip"]
+      addr = str(kwargs["ip"])
     else:
-      addr = kwargs["ip"] + ":" + str(kwargs["port"])
-    t = "Wifi SSID: {0}\nWIFI Password:{1}\nAddress: http:\\\\{2}\\".format(kwargs["ssid"],kwargs["password"],addr)
-    tsz = RendererConfig.mediumfont.getsize(t)
-    draw.text((int(screen.size[0]/2 - tsz[0]/2), 100+configicon.size[1]+50), t, font=RendererConfig.mediumfont, fill=0x00)
+      addr = str(kwargs["ip"]) + ":" + str(kwargs["port"])
+    t = [["Wifi SSID:", str(kwargs["ssid"])],
+         ["WIFI Password:",str(kwargs["password"])],
+         ["Address:", "http:\\\\{}\\".format(addr)]]
+    y = y0 + configicon.size[1] + 100
+    for l in t:
+      draw.text((x0, y), l[0], font=RendererConfig.mediumfont, fill=0x00)
+      draw.text((x0+400, y), l[1], font=RendererConfig.mediumfont, fill=0x00)
+      y = y + 80
     
     return screen
