029f1bb59a8432a771a6a6ad8a74a41895dea424
George <grob6000@users.noreply.github.com>
2021-05-18 10:02:02 +1000

    Update testap.sh

--- testap.sh
+++ testap.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo AP MODE > ./testap.log
-bash ./apmode.sh |& tee -a ./testap.log
+bash ./apmode.sh | tee -a ./testap.log
 echo SLEEP >> ./testap.log
 sleep 20
 echo CLIENT MODE >> ./testap.log
-bash ./clientmode.sh |& tee -a ./testap.log
+bash ./clientmode.sh | tee -a ./testap.log
 echo FINISHED >> ./testap.log
