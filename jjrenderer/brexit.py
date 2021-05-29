485ac26f8748e55f7b73f8379044a067aa7e60fb
George <grob6000@users.noreply.github.com>
2021-05-29 22:12:39 +1000

    daily mail

--- jjrenderer/brexit.py
+++ jjrenderer/brexit.py
@@ -19,7 +19,7 @@ class RendererBrexitClock(Renderer):
       kwargs["dstring"] = "Today"
     if len(styles) > 0:
       #r = random.randint(0,len(styles)-1) # select a random style
-      r = 1
+      r = 2
       return styles[r].doRender(self, screen, **kwargs) # pass the render down to the selected style
     else:
       return super().doRender(screen, **kwargs) # use default...
@@ -124,7 +124,51 @@ class _StyleMirror(RendererBrexitClock):
       draw.text((x,y0 + bl*40), bt, font=bongfont, fill=0x00)
     
     return screen    
+
+class _StyleDailyMail(RendererBrexitClock):
+  def doRender(self, screen, **kwargs):
+    bg = getImage("bg_dailymail")
+    screen.paste(bg)
+    draw = ImageDraw.Draw(screen)
+    datefont = getFont("arial", 18)
+    # dateline
+    x = 83
+    y = 384
+    d = ts.GetDateString(kwargs["timestamp"], lang="en", includeday=True).upper()
+    #dsz = datefont.getsize(d)
+    draw.text((x,y), d, font=datefont, fill=0x00)
+
+    # time headline
+    bgcolor = 0x00
+    textcolor = 0xFF
+    bbox = (636+20, 438+20, 636+691-20, 438+361-20)
+    pad = 50
+    T = kwargs["tstring"].upper()
+    lines = [T, ""]
+    hmax = bbox[3]-bbox[1]    
+    if len(T)>10 and " " in T:
+      lines = ts.HalfAndHalf(T)
+      hmax = int((bbox[3]-bbox[1]-pad)/2)
+    logging.debug(lines)
+    headlinefont = getFont("rockwellbold", 200)
+    y = bbox[1]
+    for l in lines:
+      if len(l)>0:
+        tsz = headlinefont.getsize(l)
+        img = Image.new("L", tsz)
+        fill(img, color=bgcolor)
+        hd = ImageDraw.Draw(img)
+        hd.text((0,0),l,font=headlinefont,fill=textcolor)
+        img = img.crop((0,headlinefont.getoffset(l)[1],img.size[0],img.size[1]))
+        s = min((bbox[2]-bbox[0])/img.size[0], hmax/img.size[1])
+        #print(s)
+        img = img.resize((int(img.size[0]*s), hmax), Image.ANTIALIAS)
+        screen.paste(img, (int((bbox[0]+bbox[2]-img.size[0])/2), y), mask=img)
+        y = y + hmax + pad
     
+    return screen
+
+        
 # automated luxury space communist style collection
 styles = []
 l = locals().copy()
