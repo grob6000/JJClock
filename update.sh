8180a166a9933452478c94d401e78fdbd6b716a4
George <grob6000@users.noreply.github.com>
2021-06-28 22:12:51 +1000

    update.sh takes parameters

--- update.sh
+++ update.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+
+# get flags
+while getopts u:p: flag
+do
+    case "${flag}" in
+        u) githubuser=${OPTARG};;
+        p) githubpass=${OPTARG};;
+    esac
+done
+
 set -x # print stuff
 
 # this should work from outside the install directory!
@@ -8,14 +18,14 @@ cd /home/pi/JJClock
 sudo systemctl stop jjclock.service
 
 # ensure git credentials are there (so bad!)
-git config --global user.name "grob6000"
-git config --global user.password "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"
+git config --global user.name "${GITHUBUSER}"
+git config --global user.password "${GITHUBPASS}"
 
 # use git to update
 git fetch --all
 tag=$(git describe --tags `git rev-list --tags --max-count=1`)
 git checkout $tag
-git pull -f
+git pull origin HEAD
 
 # place any new dependencies here
 
