3b26b5e1ea9345f24f83fd50f74de9fc97b7d1a5
George <grob6000@users.noreply.github.com>
2021-07-01 15:27:11 +1000

    Update githubcommandtest.py

--- test/githubcommandtest.py
+++ test/githubcommandtest.py
@@ -5,6 +5,18 @@ def doGitCommand(_CMD):
   githubpass = "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"
   if (_CMD[0] == "git"):
     with subprocess.Popen(_CMD, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE) as p:
-        p.communicate(githubuser + "\n" + githubpass + "\n")
-
-doGitCommand(["git", "tags"])
+      while p.poll():
+        try:
+          o,e = p.communicate(timeout=5)
+        except subprocess.TimeoutExpired:
+          print("timeout")
+        if o:
+          t = o.decode()
+          if t.startswith("Username"):
+            p.communicate(githubuser + "\n")
+            print("gave username")
+          elif t.startswith("Password"):
+            p.communicate(githubpass + "\n")
+            print("gave password")
+            
+doGitCommand(["git", "pull"])
